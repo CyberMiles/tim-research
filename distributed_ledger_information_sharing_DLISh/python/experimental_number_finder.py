0ccace0d8940c957ab540d819d7f9a14817ded74
Timothy McCallum <tpmccallum@users.noreply.github.com>
2018-03-14 07:45:14 +1000

    No longer required

--- distributed_ledger_information_sharing_DLISh/python/experimental_number_finder.py
+++
@@ -1,9 +0,0 @@
-import re
-counter = 0
-p = re.compile('[12345679]')
-for num in range(100, 9999999):
-    a = p.search(str(num))
-    if a is None:
-	    print str(num)
-	    counter = counter + 1
-print str(counter)
