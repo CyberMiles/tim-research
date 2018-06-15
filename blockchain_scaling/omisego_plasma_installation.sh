30fe12533adabcf83f695781d5432248ecb25a0b
Timothy McCallum <tpmccallum@users.noreply.github.com>
2018-06-15 18:51:49 +1000

    Fixing the eth-utils error during install
    ERROR 
    Download error on https://pypi.python.org/simple/eth-utils/: [Errno -5]

--- blockchain_scaling/omisego_plasma_installation.sh
+++ blockchain_scaling/omisego_plasma_installation.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-#OmiseGo Plasma installation
+#OmiseGo Plasma installation on Ubuntu 
 
 #Housekeeping
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apy-get autoremove -y
+sudo update-ca-certificates
 
-#Install vcpkg
-sudo apt-get install g++-7 -y
-git clone https://github.com/Microsoft/vcpkg
-cd vcpkg
-./bootstrap-vcpkg.sh
-./vcpkg integrate install
+#Install Solidity
+cd ~
+wget https://github.com/ethereum/solidity/releases/download/v0.4.18/solc-static-linux
+chmod +x ./solc-static-linux
+sudo mv solc-static-linux /usr/bin/solc
 
-#Install leveldb
-vcpkg install leveldb
+
+#Install Plasma
+git clone https://github.com/omisego/plasma-mvp.git
+cd plasma-mvp
+virtualenv env -p python3
+sudo -H make
+
+
+
+
+
 
 
 
