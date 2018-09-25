d309a17c91eee22154544198623359e6929860a8
Timothy McCallum <tpmccallum@users.noreply.github.com>
2018-09-25 12:50:38 +1000

    Making calls async instead of sync

--- bancor/generate_tests.py
+++ bancor/generate_tests.py
@@ -64,25 +64,25 @@ os.chdir(wd)
 file.write("accountA = personal.newAccount(\"asdf\");\n")
 file.write("accountB = personal.newAccount(\"asdf\");\n")
 ## Unlock the coinbase account that ships with the CyberMiles Docker private network (https://travis.readthedocs.io/en/latest/getting-started.html#use-docker)
-file.write("personal.unlockAccount(cmt.coinbase" + ", " + "\"" + "1234" + "\"" + ");\n")
+file.write("personal.unlockAccount(cmt.coinbase" + ", " + "\"" + "1234" + "\"" + ", 600, function(error, result){if(!error){console.log(result)}else{console.log(error)}});\n")
 ## Send some funds to the two new accounts
-file.write("cmt.sendTransaction({\"from\": cmt.coinbase" + ", " + "\"to\": accountA" + ", " + "\"value\": web3.toWei(50, \"cmt\")});\n")
-file.write("cmt.sendTransaction({\"from\": cmt.coinbase" + ", " + "\"to\": accountB" + ", " + "\"value\": web3.toWei(50, \"cmt\")});\n")
+file.write("cmt.sendTransaction({\"from\": cmt.coinbase" + ", " + "\"to\": accountA" + ", " + "\"value\": web3.toWei(50, \"cmt\")}, function(error, result){if(!error){console.log(result)}else{console.log(error)}});\n")
+file.write("cmt.sendTransaction({\"from\": cmt.coinbase" + ", " + "\"to\": accountB" + ", " + "\"value\": web3.toWei(50, \"cmt\")}, function(error, result){if(!error){console.log(result)}else{console.log(error)}});\n")
 ## Check all 3 account balances
-file.write("web3.fromWei(cmt.getBalance(cmt.coinbase), \"cmt\");\n")
-file.write("web3.fromWei(cmt.getBalance(accountA), \"cmt\");\n")
-file.write("web3.fromWei(cmt.getBalance(accountB), \"cmt\");\n")
+file.write("web3.fromWei(cmt.getBalance(cmt.coinbase), \"cmt\", function(error, result){if(!error){console.log(result)}else{console.log(error)}});\n")
+file.write("web3.fromWei(cmt.getBalance(accountA), \"cmt\", function(error, result){if(!error){console.log(result)}else{console.log(error)}});\n")
+file.write("web3.fromWei(cmt.getBalance(accountB), \"cmt\", function(error, result){if(!error){console.log(result)}else{console.log(error)}});\n")
 ## Unlock the two new accounts
-file.write("personal.unlockAccount(accountA " + ", " + "\"" + "asdf" + "\"" + ");\n")
-file.write("personal.unlockAccount(accountB " + ", " + "\"" + "asdf" + "\"" + ");\n")
+file.write("personal.unlockAccount(accountA " + ", " + "\"" + "asdf" + "\"" + ", 600, function(error, result){if(!error){console.log(result)}else{console.log(error)}});\n")
+file.write("personal.unlockAccount(accountB " + ", " + "\"" + "asdf" + "\"" + ", 600, function(error, result){if(!error){console.log(result)}else{console.log(error)}});\n")
 ## Save the abi from the compiled contract to a variable
 file.write("var abi = " + abi + ";\n")
 ## Save the bytecode from the compiled contract to a variable
 file.write("var bytecode = " + "\"" + "0x" + bytecode + "\";\n")
 ## Instantiate a new contract
-file.write("var newContract = web3.cmt.contract(abi);\n")
+file.write("var newContract = web3.cmt.contract(abi, function(error, result){if(!error){console.log(result)}else{console.log(error)}});\n")
 ## Deploy the new contract from account A
-file.write("var deployedContract = newContract.new(" + constructor + "{from:accountA, data: bytecode, gas:" + "\"" + "5000000" + "\"" + "});\n")
+file.write("var deployedContract = newContract.new(" + constructor + "{from:accountA, data: bytecode, gas:" + "\"" + "5000000" + "\"" + "}, function(error, result){if(!error){console.log(result)}else{console.log(error)}});\n")
 
 ## Loop through all of the read only functions in the abi 
 for item in abiDict:
@@ -90,7 +90,7 @@ for item in abiDict:
         print("Processing " + item["name"])
         if len(item["inputs"]) == 0:
         	## Call the function
-            file.write("deployedContract." + item["name"] + "();\n")
+            file.write("deployedContract." + item["name"] + "(, function(error, result){if(!error){console.log(result)}else{console.log(error)}});\n")
 
 # So now we have a single Javascript file which has a list of all of the commands you will need to deploy your contract. In addition the file has the correct syntax to execute the contrac's read only functions. More to come also...
 # The only thing left to do is to cut and paste the Javascript commands into web3 console. You can cut and paste commands one at a time, or if you are on a Mac you can pipe the whole output of the Javascript file to clipboard and run everything using a single paste action.
