8bb9d90f423e465a425c8d5a306079d0efdebcaf
Timothy McCallum <tpmccallum@users.noreply.github.com>
2018-11-13 11:06:16 +1000

    File positioning

--- uniswap/port.py
+++ uniswap/port.py
@@ -1,8 +1,8 @@
-# Place and execute this file in the src directory
+# Place and execute this file alongside the uniswap source directory
 # This will clear up and issues where ETH, Eth and eth is hard-coded in the source code.
 import os
 import subprocess
-for root, dirs, files in os.walk(os.path.join(os.getcwd(), "src")):
+for root, dirs, files in os.walk(os.path.join(os.getcwd(), "uniswap-frontend")):
     for name in files:
     	subprocess.call(["sed", "-i", "-e",  's/eth/cmt/g', os.path.join(root, name)])
     	subprocess.call(["sed", "-i", "-e",  's/ETH/CMT/g', os.path.join(root, name)])
