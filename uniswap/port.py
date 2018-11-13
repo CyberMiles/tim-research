343f6996227ffd9cffc83c03efa73d3f72ee28b7
Timothy McCallum <tpmccallum@users.noreply.github.com>
2018-11-13 11:50:52 +1000

    Unable to run this on the highest level
    Going down into src dir to test further

--- uniswap/port.py
+++ uniswap/port.py
@@ -2,7 +2,7 @@
 # This will clear up and issues where ETH, Eth and eth is hard-coded in the source code.
 import os
 import subprocess
-for root, dirs, files in os.walk(os.path.join(os.getcwd(), "uniswap-frontend")):
+for root, dirs, files in os.walk(os.path.join(os.getcwd(), "uniswap-frontend/src")):
     for name in files:
     	subprocess.call(["sed", "-i", "-e",  's/eth/cmt/g', os.path.join(root, name)])
     	subprocess.call(["sed", "-i", "-e",  's/ETH/CMT/g', os.path.join(root, name)])
