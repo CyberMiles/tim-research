b1f501fa3b004ce5331e4d16408edfa02cf60b7e
Timothy McCallum <tpmccallum@users.noreply.github.com>
2018-11-15 11:54:23 +1000

    Adding Dictionary for find replace
    Making this more robust.
    Reducing hard-coded variables
    Only replacing where chars are surrounded with quotes (double or single). This way it won't aggressively change web3.eth to web3.cmt etc.

--- uniswap/port.py
+++ uniswap/port.py
@@ -1,11 +1,29 @@
 # Place and execute this file alongside the root uniswap Github directory
 # This will clear up and issues where ETH, Eth and eth is hard-coded in the source code.
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
 import subprocess
-for root, dirs, files in os.walk(os.path.join(os.getcwd(), "uniswap-frontend", "src")):
+
+replaceDict = {
+    'ethereum': 'cybermiles',
+    'Ethereum': 'CyberMiles',
+    'eth': 'cmt',
+    'ETH': 'CMT',
+    'Eth': 'Cmt',
+    }
+
+for (root, dirs, files) in os.walk(os.path.join(os.getcwd(),
+                                   'uniswap-frontend', 'src')):
     for name in files:
-    	subprocess.call(["sed", "-i", "-e",  's/\'ethereum\'/\'cybermiles\'/g', os.path.join(root, name)])
-    	subprocess.call(["sed", "-i", "-e",  's/\'Ethereum\'/\'CyberMiles\'/g', os.path.join(root, name)])
-    	subprocess.call(["sed", "-i", "-e",  's/\'eth\'/\'cmt\'/g', os.path.join(root, name)])
-    	subprocess.call(["sed", "-i", "-e",  's/\'ETH\'/\'CMT\'/g', os.path.join(root, name)])
-    	subprocess.call(["sed", "-i", "-e",  's/\'Eth\'/\'Cmt\'/g', os.path.join(root, name)])
+        for (key, value) in replaceDict.items():
+            sedCommandSingleQuotes = "s/\\\'" + key + "\\\'/\\\'" \
+                + value + "\\\'/g"
+            sedCommandDoubleQuotes = 's/\\"' + key + '\\"/\\"' + value \
+                + '\\"/g'
+            print(sedCommandSingleQuotes)
+            print(sedCommandDoubleQuotes)
+            subprocess.call(['sed', '-i', '-e', sedCommandSingleQuotes,
+                            os.path.join(root, name)])
+            subprocess.call(['sed', '-i', '-e', sedCommandDoubleQuotes,
+                            os.path.join(root, name)])
