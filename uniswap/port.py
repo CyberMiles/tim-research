35beb835c10c3aaef02f5fe2c8e001373495acd9
Timothy McCallum <tpmccallum@users.noreply.github.com>
2018-11-15 16:00:44 +1000

    Adding semi-colon

--- uniswap/port.py
+++ uniswap/port.py
@@ -73,7 +73,7 @@ pairExchangeAndTokenAddresses()
 
 stringForAddressJsFile = ''
 stringForAddressJsFile = stringForAddressJsFile + 'const ' \
-    + networkName.upper() + ' = ' + json.dumps(addressJsData, indent=4)
+    + networkName.upper() + ' = ' + json.dumps(addressJsData, indent=4) + ";"
 print(stringForAddressJsFile)
 
 '''
