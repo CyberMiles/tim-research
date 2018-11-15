8965ce647a87f6bb9aacfaa23f031c15683c94ca
Timothy McCallum <tpmccallum@users.noreply.github.com>
2018-11-15 11:05:12 +1000

    Updating ETH to CMT script

--- uniswap/port.py
+++ uniswap/port.py
@@ -1,9 +1,11 @@
-# Place and execute this file alongside the uniswap source directory
+# Place and execute this file alongside the root uniswap Github directory
 # This will clear up and issues where ETH, Eth and eth is hard-coded in the source code.
 import os
 import subprocess
-for root, dirs, files in os.walk(os.path.join(os.getcwd(), "uniswap-frontend/src")):
+for root, dirs, files in os.walk(os.path.join(os.getcwd(), "uniswap-frontend", "src")):
     for name in files:
-    	subprocess.call(["sed", "-i", "-e",  's/eth/cmt/g', os.path.join(root, name)])
-    	subprocess.call(["sed", "-i", "-e",  's/ETH/CMT/g', os.path.join(root, name)])
-    	subprocess.call(["sed", "-i", "-e",  's/Eth/Cmt/g', os.path.join(root, name)])
+    	subprocess.call(["sed", "-i", "-e",  's/\'ethereum\'/\'cybermiles\'/g', os.path.join(root, name)])
+    	subprocess.call(["sed", "-i", "-e",  's/\'Ethereum\'/\'CyberMiles\'/g', os.path.join(root, name)])
+    	subprocess.call(["sed", "-i", "-e",  's/\'eth\'/\'cmt\'/g', os.path.join(root, name)])
+    	subprocess.call(["sed", "-i", "-e",  's/\'ETH\'/\'CMT\'/g', os.path.join(root, name)])
+    	subprocess.call(["sed", "-i", "-e",  's/\'Eth\'/\'Cmt\'/g', os.path.join(root, name)])
