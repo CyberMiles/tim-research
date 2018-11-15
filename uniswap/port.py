8f50d4f670b1d6c0d6c47a49c760c124c6236ea4
Timothy McCallum <tpmccallum@users.noreply.github.com>
2018-11-15 16:07:01 +1000

    TODOs

--- uniswap/port.py
+++ uniswap/port.py
@@ -1,3 +1,6 @@
+#TODO write address.js json directoy to the very start of the file in src/ducks/address.js
+#TODO write alternate config for start up scripts also
+
 # Place and execute this file alongside the root uniswap Github directory
 # This will clear up and issues where ETH, Eth and eth is hard-coded in the source code.
 import os
