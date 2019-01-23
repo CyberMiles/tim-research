18633c786657a204c08024d1f613ba5b0d005dd8
Timothy McCallum <tpmccallum@users.noreply.github.com>
2019-01-23 17:22:21 +1000

    Promise pending

--- reading_event_logs/code/nodeJsHtml/app/server.js
+++ reading_event_logs/code/nodeJsHtml/app/server.js
@@ -8,45 +8,68 @@ var Web3 = require('web3');
 node = 'https://mainnet.infura.io/v3/46d1afae8b55464585222887f55eab6a';
 const web3 = new Web3(new Web3.providers.HttpProvider(node));
 
+// Uniswap ABI
+var uniswapAbi = require("./public/contracts/uniswap_exchange_contract/abi.json");
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+});
+
+getCurrentBlock().then(value => blockNumber = value);
+
+async function getCurrentBlock() {
+    let response = await web3.eth.getBlockNumber();
+    return response;
+}
+
+async function getEvents(theKey, theContract, theLastIndexedBlock, theBlockNumber) {
+    let theEvents = await theContract.getPastEvents(theKey, {
+        filter: {},
+        fromBlock: theLastIndexedBlock,
+        toBlock: theBlockNumber
+    });
+    return theEvents;
+}
 
 function checkIfItemExists(theUrl) {
-	request = new XMLHttpRequest();
-	console.log("Checking URL: " + theUrl);
-	request.open('GET', theUrl, /* async = */ false);
-	request.send();
-	console.log('status code: ' + request.status);
-	if(request.status === 404){
-		return false;
-	} else{
-		return true;
-	}	
+    request = new XMLHttpRequest();
+    console.log("Checking URL: " + theUrl);
+    request.open('GET', theUrl, /* async = */ false);
+    request.send();
+    console.log('status code: ' + request.status);
+    if (request.status === 404) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
-function getAllItemsInIndex(theUrl){
-	theRequest = new XMLHttpRequest();
-	theUrl = theUrl + "?size=1000"
-	console.log("Checking URL: " + theUrl);
-	theRequest.open('GET', theUrl, /* async = */ false);
-	theRequest.send();
-	if (theRequest.status >= 200 && theRequest.status < 300) {
-		//console.log(theRequest.responseText);
-		return theRequest.responseText;
-	} else {
-		console.log(theRequest.status);
-	}
+function getAllItemsInIndex(theUrl) {
+    theRequest = new XMLHttpRequest();
+    theUrl = theUrl + "?size=1000"
+    console.log("Checking URL: " + theUrl);
+    theRequest.open('GET', theUrl, /* async = */ false);
+    theRequest.send();
+    if (theRequest.status >= 200 && theRequest.status < 300) {
+        console.log(theRequest.responseText);
+        console.log("Fetched all of the items!")
+        return theRequest.responseText;
+    } else {
+        console.log(theRequest.status);
+    }
 }
 
 
-function bulkIngest(bulkData){
-client.bulk( bulkData, function(error, response) {
-    if (error) {
-      console.error(error);
-      return;
-    }
-    else {
-    console.log(response);  //  I don't recommend this but I like having my console flooded with stuff.  It looks cool.  Like I'm compiling a kernel really fast.
-    }
-  });
+function bulkIngest(bulkData) {
+    client.bulk(bulkData, function(error, response) {
+        if (error) {
+            console.error(error);
+            return;
+        } else {
+            console.log(response); //  I don't recommend this but I like having my console flooded with stuff.  It looks cool.  Like I'm compiling a kernel really fast.
+        }
+    });
 
 }
 
@@ -56,11 +79,11 @@ function loadContracts(uniswapAbi) {
     var dataToLoad = {};
     var eventsToLoad = {};
     url = "http://localhost:9200/uniswap_exchange_register/_all/";
-    for(var key in uniswapAbi){
-		if (uniswapAbi[key]["type"] === "event"){
-			eventsToLoad[uniswapAbi[key].name] = uniswapAbi[key]
-		}
-	}
+    for (var key in uniswapAbi) {
+        if (uniswapAbi[key]["type"] === "event") {
+            eventsToLoad[uniswapAbi[key].name] = uniswapAbi[key]
+        }
+    }
 
     var uniswapAddresses = require("./public/contracts/uniswap_exchange_contract/addresses.json");
     //console.log(JSON.stringify(uniswapAddresses));
@@ -71,63 +94,68 @@ function loadContracts(uniswapAbi) {
             console.log("Processing: " + uniswapAddresses[key]);
             theUrlWithKey = url + uniswapAddresses[key];
             var contractExists = checkIfItemExists(theUrlWithKey);
-            if(contractExists == false){
-            	console.log("Item does not exist, let's add it ...")
-            	theId = {};
-            	actionDescription = {};
-            	theId["_id"] = uniswapAddresses[key];
-            	theId["_type"] = "exchange";
-            	theId["_index"] = 'uniswap_exchange_register';
-            	actionDescription["index"] = theId;
-            	theBodyArray.push(actionDescription);
-            	theDocumentToIndex = {};
-            	theDocumentToIndex["name"] = key;
-            	theDocumentToIndex["lastIndexedBlock"] = 0;
-            	theDocumentToIndex["beingHarvested"] = false;
-            	theDocumentToIndex["events"] = eventsToLoad;
-            	theBodyArray.push(theDocumentToIndex);
+            if (contractExists == false) {
+                console.log("Item does not exist, let's add it ...")
+                theId = {};
+                actionDescription = {};
+                theId["_id"] = uniswapAddresses[key];
+                theId["_type"] = "exchange";
+                theId["_index"] = 'uniswap_exchange_register';
+                actionDescription["index"] = theId;
+                theBodyArray.push(actionDescription);
+                theDocumentToIndex = {};
+                theDocumentToIndex["name"] = key;
+                theDocumentToIndex["lastIndexedBlock"] = 0;
+                theDocumentToIndex["beingHarvested"] = false;
+                theDocumentToIndex["events"] = eventsToLoad;
+                theBodyArray.push(theDocumentToIndex);
 
             }
         }
     }
-    if(theBodyArray.length > 0){
-    	bulkData["body"] = theBodyArray;
-    	console.log("Adding the following data to the uniswap_exchange_register")
-    	console.log(JSON.stringify(bulkData));
-    	bulkIngest(bulkData);
+    if (theBodyArray.length > 0) {
+        bulkData["body"] = theBodyArray;
+        console.log("Adding the following data to the uniswap_exchange_register")
+        console.log(JSON.stringify(bulkData));
+        bulkIngest(bulkData);
     }
-    
-}
-
-function harvestContracts(uniswapAbi){
-	console.log("Harvesting contracts...");
-	blockNumber =  await web3.eth.getBlockNumber();  
-	console.log("Current block number = " + blockNumber);
-	url = "http://localhost:9200/uniswap_exchange_register/_search";
-	var theItems = getAllItemsInIndex(url);
-	for(var ii = 0; ii < theItems.hits.hits.length; ii++){
-		console.log("Processing: " + theItems.hits.hits[ii]._source.name +  " at address: " + theItems.hits.hits[ii]._id);
-		var lastIndexedBlock = theItems.hits.hits[ii]._source.lastIndexedBlock;
-		var aContract = new web3.eth.Contract(uniswapAbi, theItems.hits.hits[ii]._id);
-		for(var iii = 0; iii < theItems.hits.hits[ii]._source.events.length; iii++){
-			if(theItems.hits.hits[ii]._source.lastIndexedBlock == 0 || theItems.hits.hits[ii]._source.lastIndexedBlock < blockNumber){
-				var events = aContract.getPastEvents(theItems.hits.hits[ii]._source.events[iii], {filter: {}, fromBlock: theItems.hits.hits[ii]._source.lastIndexedBlock, toBlock: blockNumber});
-				console.log(events);
-				//theItems.hits.hits[ii]._source.events
-			}
-		}
-		
-	}
 
 }
 
-// Uniswap ABI
-var uniswapAbi = require("./public/contracts/uniswap_exchange_contract/abi.json");
-var elasticsearch = require('elasticsearch');
-var client = new elasticsearch.Client({
-    host: 'localhost:9200',
-    log: 'trace'
-});
+function harvestContracts(uniswapAbi) {
+    console.log("Harvesting contracts...");
+
+    console.log("Current block number = " + blockNumber);
+    url = "http://localhost:9200/uniswap_exchange_register/_search";
+    var theItems = JSON.parse(getAllItemsInIndex(url));
+    console.log("These are the items as seen in the harvest contracts function!")
+    for (var ii = 0; ii < theItems.hits.hits.length; ii++) {
+        console.log("Processing: " + theItems.hits.hits[ii]._source.name + " at address: " + theItems.hits.hits[ii]._id);
+        var lastIndexedBlock = theItems.hits.hits[ii]._source.lastIndexedBlock;
+        console.log("The last indexed block for " + theItems.hits.hits[ii]._source.name + " is " + lastIndexedBlock);
+        var aContract = new web3.eth.Contract(uniswapAbi, theItems.hits.hits[ii]._id);
+        console.log(lastIndexedBlock);
+        console.log(blockNumber);
+        var theEvents = theItems.hits.hits[ii]._source.events;
+        console.log("Events:");
+        for (var key in theEvents) {
+            if (theEvents.hasOwnProperty(key)) {
+                if (lastIndexedBlock == 0 || lastIndexedBlock < blockNumber) {
+                    console.log("We have some harvesting to do ...");
+                    console.log("Harvesting " + key);
+                    var theEvents = getEvents(key, aContract, lastIndexedBlock, blockNumber);
+                    console.log(theEvents);
+                    //theItems.hits.hits[ii]._source.events
+
+                }
+            }
+
+        }
+
+    }
+}
+
+
 
 const app = express();
 
